574c86822e41199471cc356b013b8ad78d5f3742
JD_Rodrigues <jdevproject01@gmail.com>
2022-08-10 15:54:22 -0300

    padronização dos nomes de classes CSS

--- scripts.js
+++
@@ -1,6 +0,0 @@
-const menuMobile = document.querySelector('.js-menu__mobile')
-const menuBurguerBtn = document.querySelector('.js-menu-burguer__btn')
-
-menuBurguerBtn.addEventListener('click', ()=>{
-   console.log('oi')
-})
