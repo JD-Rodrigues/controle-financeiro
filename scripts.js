9415726a5795b0b8136775a6af06bf4b13827504
JD_Rodrigues <jdevproject01@gmail.com>
2022-08-09 21:59:28 -0300

    Criação de menu mobile

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const menuMobile = document.querySelector('.js-menu__mobile')
+const menuBurguerBtn = document.querySelector('.js-menu-burguer__btn')
+
+menuBurguerBtn.addEventListener('click', ()=>{
+   console.log('oi')
+})
